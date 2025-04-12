e35d4c71e7e3cc93e279c2e8b452e72653d6f65b
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-12 09:59:52 +0200

    `Added CssBaseline and wrapped Router in React.Fragment`

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,14 +5,18 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <Router>
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-    </Router>
+      </Router>
+    </React.Fragment>
   );
 }
