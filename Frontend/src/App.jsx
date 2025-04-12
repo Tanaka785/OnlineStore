6d41647eb0a5e237cd66e9edda40cd7a4211912b
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-12 13:47:01 +0200

    `Added @fontsource/roboto dependency and imported it in App.jsx`

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '@fontsource/roboto'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
