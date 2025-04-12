043a375f59f3859e9a5e4f6a3c600a974fcf4339
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-12 13:38:10 +0200

    "Added sx prop with fontWeight 'bold' to Typography component in NavigationBar"

--- Frontend/src/components/common/Navigation.jsx
+++ Frontend/src/components/common/Navigation.jsx
@@ -220,6 +220,7 @@ export default function NavigationBar() {
             variant="h6"
             noWrap
             component="div"
+            sx={{ fontWeight: "bold" }}
           >
             WEBVIBES
           </Typography>
