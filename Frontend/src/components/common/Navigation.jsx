4148d30e5f95d0c8aa6a65dc7f9b8be03550fe34
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-12 16:15:11 +0200

    `Added Favorite icon and removed Mail and Notifications icons from Navigation component`

--- Frontend/src/components/common/Navigation.jsx
+++ Frontend/src/components/common/Navigation.jsx
@@ -27,6 +27,8 @@ import {
   Notifications as NotificationsIcon,
   MoreVert as MoreIcon,
   ShoppingCart,
+  Favorite as FavoriteIcon,
+  Favorite
 } from '@mui/icons-material';
 
 // MUI Styling
@@ -272,19 +274,14 @@ export default function NavigationBar() {
           <Box sx={{ flexGrow: 1 }} />
 
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
+            <IconButton color="inherit">
+              <Badge color="error">
+                <Favorite />
               </Badge>
             </IconButton>
             
             <IconButton color="inherit">
-              <Badge badgeContent={3} color="error">
+              <Badge color="error">
                 <ShoppingCart />
               </Badge>
             </IconButton>
