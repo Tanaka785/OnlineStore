0ced2ddb61b325d0e2ec53bc630dc1dfa966c699
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-13 14:27:34 +0200

    sets the width of the search textfield to 600px from "md" screen size up to xlg

--- Frontend/src/components/common/Navigation.jsx
+++ Frontend/src/components/common/Navigation.jsx
@@ -43,12 +43,9 @@ const Search = styled("div")(({ theme }) => ({
   marginRight: theme.spacing(2),
   marginLeft: 0,
   width: "100%",
-  [theme.breakpoints.up("sm")]: {
+  [theme.breakpoints.up("md")]: {
     marginLeft: theme.spacing(3),
     width: "600px",
-  },
-  [theme.breakpoints.up("md")]: {
-    width: "680px",
   },
 }));
 
