ea2d2e89d0d543ba4b4bc3a9fa1f9364798708b7
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-13 15:46:31 +0200

    makes the box containing the logo and the logo text , a clickable link

--- Frontend/src/components/common/Navigation.jsx
+++ Frontend/src/components/common/Navigation.jsx
@@ -227,49 +227,51 @@ export default function NavigationBar() {
             </Box>
           </Drawer>
 
-          <Box sx={{ display: "flex"}}>
-            <Avatar
-              alt="Logo"
-              src=""
-              sx={{
-                backgroundImage: "url(/Logo.png)",
-                backgroundSize: "200%",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                mr: 1,
-                width: {
-                  xs: 20,
-                  sm: 30,
-                },
-                height: {
-                  xs: 20,
-                  sm: 30,
-                },
-                marginBottom: {
-                  xs: 0.5,
-                  sm: 0,
-                },
-              }}
-            />
+          <Link>
+            <Box sx={{ display: "flex" }}>
+              <Avatar
+                alt="Logo"
+                src=""
+                sx={{
+                  backgroundImage: "url(/Logo.png)",
+                  backgroundSize: "200%",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  mr: 1,
+                  width: {
+                    xs: 20,
+                    sm: 30,
+                  },
+                  height: {
+                    xs: 20,
+                    sm: 30,
+                  },
+                  marginBottom: {
+                    xs: 0.5,
+                    sm: 0,
+                  },
+                }}
+              />
 
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{
-                fontWeight: "bold",
-                fontSize: {
-                  xs: "1.2rem",
-                  sm: "1.55rem",
-                },
-                marginTop: {
-                  xs: 0,
-                  sm: 0.3,
-                },
-              }}
-            >
-              WEBVIBES
-            </Typography>
-          </Box>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: {
+                    xs: "1.2rem",
+                    sm: "1.55rem",
+                  },
+                  marginTop: {
+                    xs: 0,
+                    sm: 0.3,
+                  },
+                }}
+              >
+                WEBVIBES
+              </Typography>
+            </Box>
+          </Link>
 
           <Search sx={{ display: { xs: "none", md: "block" } }}>
             <SearchIconWrapper>
