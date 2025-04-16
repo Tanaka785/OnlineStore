07a9e6552d251c6ff1a4124ab3ee3e4a6b7a3f62
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-16 14:03:34 +0200

    `Added AppBar and Box components to Home page`

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import AppBar from "../components/common/Navigation";
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+
 function Home() {
     return (
-        <>
-            <h1>
-                Hello World!
-            </h1>
-        </>
+        <Box sx={{ display: "flex", height: '100vh' }}>
+            <CssBaseline />
+            <AppBar />
+        </Box>
     )
 };
 
