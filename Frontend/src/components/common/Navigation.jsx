cdc1baa58229ada983bdcab6ec8028fc87b1b97e
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-16 14:48:53 +0200

    `Removed navigation menu and login button from NavigationAppBar component`

--- Frontend/src/components/common/Navigation.jsx
+++ Frontend/src/components/common/Navigation.jsx
@@ -21,19 +21,7 @@ export default function NavigationAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <StyledAppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          
         </Toolbar>
       </StyledAppBar>
     </Box>
