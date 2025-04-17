4cc22f3a5407724234e873feb4840bfe62517baf
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-17 16:27:11 +0200

    Add custom theme with Inter font

---
+++ Frontend/src/theme/theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Inter', sans-serif",
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+  },
+});
+
+export default theme;
