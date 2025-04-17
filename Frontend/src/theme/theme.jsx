855318ae289b51d7f1b930ce1b0bd1dd5bffe3af
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-17 16:56:29 +0200

    Update typography styles in theme.jsx

--- Frontend/src/theme/theme.jsx
+++ Frontend/src/theme/theme.jsx
@@ -3,9 +3,25 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
   typography: {
     fontFamily: "'Inter', sans-serif",
-        fontWeightRegular: 400,
-        fontWeightMedium: 600,
-        fontWeightBold: 900,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    fontWeightBold: 900,
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "2rem",
+    },
+    h6: {
+      fontSize: "1.125rem",
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+    body2: {
+      fontSize: "0.875rem",
+    },
   },
 });
 
