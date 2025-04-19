bf08cf56d80094e81260cd3b2a41edb5e0156546
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 00:39:28 +0200

    import categories.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import NavigationAppBar from "../components/common/Navigation";
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
+import Categories from "../components/common/Categories";
 function Home() {
     return (
         <Box>
             <CssBaseline />
             <NavigationAppBar />
+            <Categories/>
         </Box>
     )
 };
