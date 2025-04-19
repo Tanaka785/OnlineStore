6d72911bd3093d4dffed826cc180762b09f62ba2
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-19 22:35:46 +0200

    adds searchfield component.

---
+++ Frontend/src/components/common/SearchField.jsx
@@ -0,0 +1,22 @@
+import { Box, TextField, InputAdornment } from "@mui/material";
+import { Search as SearchIcon } from "@mui/icons-material";
+import { useTheme } from "@emotion/react";
+export default function SearchField() {
+    const theme = useTheme();
+    return (
+      <Box sx={{ flex: 1, minWidth: 0, marginInline: theme.spacing(1) }}>
+        <TextField
+          fullWidth
+          slotProps={{
+            input: {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon sx={{ minWidth: 30, minHeight: 30 }} />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </Box>
+    );
+}
