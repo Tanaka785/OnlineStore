13cdbb88e77d526463f7757ef93694ea60fce464
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 01:10:02 +0200

    adds a category "Explore".

--- Frontend/src/components/common/Categories.jsx
+++ Frontend/src/components/common/Categories.jsx
@@ -1,7 +1,35 @@
-import { Box } from "@mui/material";
-
+import { Box, useTheme, Typography, Tooltip, List, ListItem, ListItemText } from "@mui/material";
 export default function Categories() {
-    return (
-        <>Hello</>
-    )
-};
+  const theme = useTheme();
+  return (
+    <Box sx={{ marginInline: theme.spacing(3), display: "flex" }}>
+      <Tooltip
+      arrow
+        title={
+          <List>
+            <ListItem>
+              <ListItemText primary="For You" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Funny T-Shirts" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Vintage Posters" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Music Posters" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Fresh Finds" />
+            </ListItem>
+            <ListItem>
+              <ListItemText  primary="Fan Art"/>
+            </ListItem>
+          </List>
+        }
+      >
+        <Typography>Explore</Typography>
+      </Tooltip>
+    </Box>
+  );
+}
