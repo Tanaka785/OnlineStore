7652133dcd424377ce0aaa44c8b9311550770571
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-19 23:21:01 +0200

    removes the searchfield component from home.jsx.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import NavigationAppBar from "../components/common/Navigation";
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import SearchField from "../components/common/SearchField";
 function Home() {
     return (
-        <Box sx={{  height: '100vh' }}>
+        <Box>
             <CssBaseline />
             <NavigationAppBar />
-            <SearchField/>
         </Box>
     )
 };
