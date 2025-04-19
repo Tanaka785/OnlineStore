d3c09edb6f2909ad6c366ae0d4f0a237e92af9be
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 00:41:14 +0200

    fix the import for navigationappbar

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import NavigationAppBar from "../components/common/Navigation";
+import NavigationAppBar from "../components/common/Navigation/Navigation";
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Categories from "../components/common/Categories";
