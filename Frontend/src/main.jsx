d9ab9829109a259c1d868bc62f787ae741b63776
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 11:49:06 +0200

    remove the import for index.css

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import "@fontsource/inter";
-import "./index.css";
 import App from "./App.jsx";
 import theme from "./theme/theme.jsx";
 
