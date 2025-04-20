da780c3b671c6a1cca71d9be5cbefb1bd96c83da
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 10:05:01 +0200

    styles the divider box.

--- Frontend/src/components/common/Divider.jsx
+++ Frontend/src/components/common/Divider.jsx
@@ -1,5 +1,15 @@
 import { Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 export default function Divider() {
-    return <>Hello</>
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        height: "3.6rem",
+        backgroundColor: "lightgray",
+        marginTop: theme.spacing(1.5),
+      }}
+    />
+  );
 }
