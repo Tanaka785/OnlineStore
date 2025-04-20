f7ec4c61b2dbfa8d16ff4827e4922b0611eda1cc
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-20 09:53:21 +0200

    import Divider.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,12 +2,15 @@ import NavigationAppBar from "../components/common/Navigation/Navigation";
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Categories from "../components/common/Categories";
+import Divider from "../components/common/Divider";
+
 function Home() {
     return (
         <Box>
             <CssBaseline />
             <NavigationAppBar />
-            <Categories/>
+            <Categories />
+            <Divider/>
         </Box>
     )
 };
