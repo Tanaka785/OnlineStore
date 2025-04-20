2830e946170a5d0081f3c4a5e5edc6da710d1e7e
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-21 00:34:04 +0200

    imports Banner into home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Categories from "../components/common/Categories";
 import Divider from "../components/common/Divider";
+import Banner from "../components/common/Banner";
 
 function Home() {
     return (
@@ -10,7 +11,8 @@ function Home() {
             <CssBaseline />
             <NavigationAppBar />
             <Categories />
-            <Divider/>
+            <Divider />
+            <Banner/>
         </Box>
     )
 };
