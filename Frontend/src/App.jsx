e5f38f6911542e438695766cd8a54e69090b2819
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-21 12:38:56 +0200

    adds /lists route.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
 import Selling from "./pages/Selling";
+import Wishlists from "./pages/Login";
 import {
   BrowserRouter as Router,
   Routes,
@@ -23,6 +24,7 @@ export default function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/about/selling" element={<Selling />} />
+          <Route path="/lists" element={<Selling />} />
         </Routes>
       </Router>
     </React.Fragment>
