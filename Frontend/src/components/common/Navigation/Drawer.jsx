c0eedbb7a722937dca5ddfe9477470980f11ba21
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-23 07:59:05 +0200

    defines styledDrawer to customize the mui drawer.

--- Frontend/src/components/common/Navigation/Drawer.jsx
+++ Frontend/src/components/common/Navigation/Drawer.jsx
@@ -1,8 +1,11 @@
 import { Drawer } from "@mui/material";
 import { useTheme } from "@emotion/react";
 
+const StyledDrawer = styled(Drawer)(({ theme }) => ({
+  // padding: '14px',
+}));
 
 export default function Drawer() {
-    const theme = useTheme();
-  return <>Drawer</>;
+  const theme = useTheme();
+  return <StyledDrawer></StyledDrawer>;
 }
