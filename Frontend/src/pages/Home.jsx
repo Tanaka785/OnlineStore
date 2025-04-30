3761165c75f6055c7b021e1315662619c1bae370
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-30 11:55:34 +0200

    `Removed unused import and added file extension to Categories component import`

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import NavigationAppBar from "../components/common/Navigation/Navigation";
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Categories from "../components/common/categories/Categories";
 import Divider from "../components/common/Divider";
 import Banner from "../components/common/Banner";
-
+import Categories from "../components/common/categories/Categories.jsx";
 function Home() {
     return (
         <Box>
