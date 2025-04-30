9f14c2e033e229196d4e29ec43fe1bf51f6744df
Tanaka785 <tanakachanetsa204@gmail.com>
2025-04-30 15:22:00 +0200

    `Removed two categories from the categories list`

--- Frontend/src/components/common/categories/Categories.js
+++ Frontend/src/components/common/categories/Categories.js
@@ -7,6 +7,4 @@ export const categories = [
   { name: "Wearable Tech", avatar: "/logo.png" },
   { name: "Smart Home Devices", avatar: "/logo.png" },
   { name: "Branded Apparel", avatar: "/logo.png" },
-  { name: "Stickers & Decals", avatar: "/logo.png" },
-  { name: "Mugs & Drinkware", avatar: "/logo.png" },
 ];
