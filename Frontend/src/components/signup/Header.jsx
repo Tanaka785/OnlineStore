be7fb77dd01c2a929f48e7f9ccca43db06a044c6
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-02 23:40:05 +0200

    aligns everything in the header at vertical center.

--- Frontend/src/components/signup/Header.jsx
+++ Frontend/src/components/signup/Header.jsx
@@ -1,9 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
+import Logo from "../common/logo/Logo";
+import { ShoppingBag } from "@mui/icons-material";
 
 export default function Header() {
   return (
-    <Box sx={{ display: "flex", marginTop: 5, justifyContent: "center" }}>
-      Header
+    <Box
+      sx={{
+        display: "flex",
+        marginTop: 5,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Logo />
+      <TextField />
+      <ShoppingBag />
     </Box>
   );
 }
