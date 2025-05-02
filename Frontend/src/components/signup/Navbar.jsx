db84586d43fa0c73241b41c7dad47b79b01bc64f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-02 22:31:55 +0200

    adds two links to the navbar.

--- Frontend/src/components/signup/Navbar.jsx
+++ Frontend/src/components/signup/Navbar.jsx
@@ -1,3 +1,12 @@
+import { AppBar, Link, Box } from "@mui/material";
+
 export default function SignupNavbar() {
-  return <>SignupNavbar</>;
+  return (
+    <AppBar>
+      <Box>
+        <Link>Sell Your Designs</Link>
+        <Link>Login</Link>
+      </Box>
+    </AppBar>
+  );
 }
