057b941416a802f40c67fbb87477a4f6dc0e576c
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-02 22:26:43 +0200

    imports and renders signupNavbar component in Signup.jsx page.

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,5 +1,10 @@
+import { Box } from "@mui/material";
+import SignupNavbar from "../components/signup/Navbar";
+
 export default function Signup() {
     return (
-        <>Signup</>
+        <Box>
+            <SignupNavbar/>
+        </Box>
     )
 }
