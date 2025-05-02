cc45ceee6cb6c6030fc17584643d884754eeea22
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-02 23:34:15 +0200

    defines Header component and imports it in Signup.jsx page.

---
+++ Frontend/src/components/signup/Header.jsx
@@ -0,0 +1,5 @@
+import { Box } from "@mui/material";
+
+export default function Header() {
+  return <Box sx={{ marginTop: 5, justifyContent: 'center' }}>Header</Box>;
+}
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import SignupNavbar from "../components/signup/Navbar";
+import Header from "../components/signup/Header";
 
 export default function Signup() {
-    return (
-        <Box>
-            <SignupNavbar/>
-        </Box>
-    )
+  return (
+    <Box>
+      <SignupNavbar />
+      <Header />
+    </Box>
+  );
 }
