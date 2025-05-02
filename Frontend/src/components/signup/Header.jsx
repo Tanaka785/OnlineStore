1609367d83466538b1452aa2c31432cb79d6d64c
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-02 23:35:46 +0200

    aligns items in the header center.

--- Frontend/src/components/signup/Header.jsx
+++ Frontend/src/components/signup/Header.jsx
@@ -1,5 +1,9 @@
 import { Box } from "@mui/material";
 
 export default function Header() {
-  return <Box sx={{ marginTop: 5, justifyContent: 'center' }}>Header</Box>;
+  return (
+    <Box sx={{ display: "flex", marginTop: 5, justifyContent: "center" }}>
+      Header
+    </Box>
+  );
 }
