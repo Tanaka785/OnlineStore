373dd173d5bf541a1630789c7a24c58134164978
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 13:55:35 +0200

    `Refactor Typography styles in Promo component`

--- Frontend/src/components/signup/Promo.jsx
+++ Frontend/src/components/signup/Promo.jsx
@@ -14,10 +14,15 @@ export default function Promo() {
         marginTop: 12,
       }}
     >
-      <Typography variant="h5">Join Webvibes</Typography>
       <Typography
         variant="body1"
-        sx={{ width: "50%", color: theme.palette.grey[800] }}
+        sx={{ letterSpacing: -1, fontSize: "1.5rem" }}
+      >
+        Join Webvibes
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{ width: "70%", color: theme.palette.grey[800] }}
       >
         Sign up as a customer for 25% off your first order. Your coupon will be
         emailed after sign up.
