aa39ed356f723549cf92ecd1fc611fda83199da9
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 13:49:16 +0200

    `Added promotional content to Promo component`

--- Frontend/src/components/signup/Promo.jsx
+++ Frontend/src/components/signup/Promo.jsx
@@ -1,5 +1,22 @@
 import { Box } from "@mui/material";
 
 export default function Promo() {
-  return <Box>Hello</Box>;
+  return (
+    <Box
+      gap={2}
+      sx={{
+        placeItems: "center",
+        marginTop: 12,
+      }}
+    >
+      <Typography variant="h5">Join Webvibes</Typography>
+      <Typography
+        variant="body1"
+        sx={{ width: "35%", color: theme.palette.grey[800] }}
+      >
+        Sign up as a customer for 25% off your first order. Your coupon will be
+        emailed after sign up.
+      </Typography>
+    </Box>
+  );
 }
