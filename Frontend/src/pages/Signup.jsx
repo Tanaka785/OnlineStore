5e695632072c5adbacf0951c21d4bca4c09c12a5
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 13:38:46 +0200

    `Updated Signup.jsx: added gap to StyledBox and set width on Typography`

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -19,13 +19,14 @@ export default function Signup() {
         <Header />
       </StyledBox>
       <StyledBox
+        gap={3}
         sx={{
           placeItems: "center",
           marginTop: 12,
         }}
       >
         <Typography variant="h5">Join Webvibes</Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" sx={{ width: "35%" }}>
           Sign up as a customer for 25% off your first order. Your coupon will
           be emailed after sign up.
         </Typography>
