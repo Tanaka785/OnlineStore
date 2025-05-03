c0212b79c053fd7828285ca9c6845411c5d11eb5
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 14:51:15 +0200

    `Refactor UserCategory component to use styled components and MUI theme`

--- Frontend/src/components/signup/UserCategory.jsx
+++ Frontend/src/components/signup/UserCategory.jsx
@@ -1,10 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, alpha, styled } from "@mui/material";
+import { useTheme } from "@emotion/react";
+
+const SelectableBox = styled(Box)(({ theme }) => ({
+  border: `1px solid ${alpha(theme.palette.divider, 0.2)}`,
+}));
 
 export default function UserCategory() {
   return (
     <Box sx={{ display: "flex", marginTop: 3 }}>
-      <Box>Hello</Box>
-      <Box>Hello</Box>
+      <SelectableBox>Hello</SelectableBox>
+      <SelectableBox>Hello</SelectableBox>
     </Box>
   );
 }
