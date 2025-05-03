64f623e1ff9157423d9d5ccf198ff0694cfb7960
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 15:01:29 +0200

    `Added height styles to SelectableBox and CategoryBox components in UserCategory.jsx`

--- Frontend/src/components/signup/UserCategory.jsx
+++ Frontend/src/components/signup/UserCategory.jsx
@@ -4,6 +4,7 @@ import { useTheme } from "@emotion/react";
 const SelectableBox = styled(Box)(({ theme }) => ({
   border: `1px solid ${alpha(theme.palette.divider, 0.2)}`,
   width: "50%",
+  height: "100%",
 }));
 
 export default function UserCategory() {
@@ -17,6 +18,7 @@ export default function UserCategory() {
         marginTop: 3,
         border: "1px solid red",
         width: "36%",
+        height: "50px",
         justifyContent: "center",
         alignItems: "center",
       }}
