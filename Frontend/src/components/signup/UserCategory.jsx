f2636dfdc7ed0cd555a156be8390c3ae260bb4de
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 15:37:17 +0200

    `Update gap value in SelectableBox styles from 3 to 5`

--- Frontend/src/components/signup/UserCategory.jsx
+++ Frontend/src/components/signup/UserCategory.jsx
@@ -10,7 +10,7 @@ const SelectableBox = styled(Box)(({ theme }) => ({
   height: "100%",
   paddingLeft: theme.spacing(2),
   paddingTop: theme.spacing(2),
-  gap: 3,
+  gap: 5,
 }));
 
 export default function UserCategory() {
