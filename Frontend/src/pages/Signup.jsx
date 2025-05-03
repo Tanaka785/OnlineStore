c842fd76d139694b12ef79bc7a0c485ed5fa2e7e
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 13:42:31 +0200

    `Update Signup.jsx: add color to Typography component`

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -26,7 +26,10 @@ export default function Signup() {
         }}
       >
         <Typography variant="h5">Join Webvibes</Typography>
-        <Typography variant="body1" sx={{ width: "35%" }}>
+        <Typography
+          variant="body1"
+          sx={{ width: "35%", color: theme.palette.grey[800] }}
+        >
           Sign up as a customer for 25% off your first order. Your coupon will
           be emailed after sign up.
         </Typography>
