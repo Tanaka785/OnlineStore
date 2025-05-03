4fed613c15c073b751be8aab84eef5a2c7405130
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-03 11:20:43 +0200

    `Update Header component with new imports and styles`

--- Frontend/src/components/signup/Header.jsx
+++ Frontend/src/components/signup/Header.jsx
@@ -1,15 +1,19 @@
 import { Box, TextField } from "@mui/material";
 import Logo from "../common/logo/Logo";
 import { ShoppingBag } from "@mui/icons-material";
-
+import { useTheme } from "@emotion/react";
+import { alpha } from "@mui/material";
 export default function Header() {
+  const theme = useTheme();
+  
   return (
     <Box
       sx={{
         display: "flex",
-        marginTop: 5,
+        marginTop: 16,
         justifyContent: "center",
         alignItems: "center",
+        border: ` 1px solid ${alpha(theme.palette.divider, 0.05)}`,
       }}
     >
       <Logo />
