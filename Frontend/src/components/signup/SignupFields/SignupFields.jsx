dd180ebb87b84600c53f12a50c8bfec4a4f1fd83
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-07 17:37:55 +0200

    `Update SignupFields component: add border to grid container`

--- Frontend/src/components/signup/SignupFields/SignupFields.jsx
+++ Frontend/src/components/signup/SignupFields/SignupFields.jsx
@@ -3,7 +3,7 @@ import StandardTextField from "./StandardTextField";
 
 export default function SignupFields() {
   return (
-    <Box display={"grid"} sx={{ width: "100%" }}>
+    <Box display={"grid"} sx={{ width: "70%", border: "1px solid blue" }}>
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Password" />
