81c4b0cc25fccbdf601bb4455e99eecd163510f5
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-07 21:23:12 +0200

    changes marginTop to marginBlock

--- Frontend/src/components/signup/SignupFields/SignupFields.jsx
+++ Frontend/src/components/signup/SignupFields/SignupFields.jsx
@@ -3,7 +3,7 @@ import StandardTextField from "./StandardTextField";
 
 export default function SignupFields() {
   return (
-    <Box display={"grid"} sx={{ width: "70%", marginTop: { md: 4 } }}>
+    <Box display={"grid"} sx={{ width: "70%", marginBlock: { md: 4 } }}>
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Password" />
