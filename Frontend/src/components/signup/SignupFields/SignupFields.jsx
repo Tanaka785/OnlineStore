822eb27c9fef530cfd25058a0b879bfd114eed03
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-07 21:26:12 +0200

    sets gap of 8 for the spacing of the elements inside the containing box in signupfield.jsx

--- Frontend/src/components/signup/SignupFields/SignupFields.jsx
+++ Frontend/src/components/signup/SignupFields/SignupFields.jsx
@@ -3,7 +3,7 @@ import StandardTextField from "./StandardTextField";
 
 export default function SignupFields() {
   return (
-    <Box display={"grid"} sx={{ width: "70%", marginBlock: { md: 4 } }}>
+    <Box display={"grid"} gap={8} sx={{ width: "70%", marginBlock: { md: 4 } }}>
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Email" />
       <StandardTextField placeholder="Password" />
