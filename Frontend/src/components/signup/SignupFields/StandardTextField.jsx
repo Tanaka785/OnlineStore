a6153cd3c02341de3ab0348dfab8d23cc491fbe6
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-08 15:18:39 +0200

    `Added CSS styles for MUI TextField variants`

--- Frontend/src/components/signup/SignupFields/StandardTextField.jsx
+++ Frontend/src/components/signup/SignupFields/StandardTextField.jsx
@@ -3,12 +3,20 @@ import { useTheme } from "@emotion/react";
 
 export default function StandardTextField(props) {
   const theme = useTheme();
-
+  console.log(theme);
   return (
     <TextField
       variant="standard"
       sx={{
-        color: theme.palette.grey[5],
+        "& .MuiInput-underline:before": {
+          borderBottomColor: "#d3d3d3",
+        },
+        "& .MuiInput-underline:hover:before": {
+          borderBottomColor: theme.palette.secondary.main,
+        },
+        "& .MuiInput-underline:after": {
+          borderBottomColor: theme.palette.success.main,
+        },
         paddingBlock: 2,
       }}
       {...props}
