48e5525b95d42bff35e54cf119bd51cbdfc340b8
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-11 12:12:31 +0200

    `Update dependencies to React 18.2.0 and React DOM 18.2.0`

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import Homepage from "./pages/Home";
+import { CssBaseline, Box } from "@mui/material";
+
 function App() {
-  return <Homepage />;
+  return (
+    <Box>
+      <CssBaseline />
+      <Homepage />
+    </Box>
+  );
 }
 
 export default App;
