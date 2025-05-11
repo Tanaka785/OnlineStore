eefb9eaf430b8c7a4fca0c76e5a47cad9fb3c470
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-11 16:11:02 +0200

    `Added navLinks array and updated NavLinks component`

--- Frontend/src/components/shared/Navbar/NavLinks.jsx
+++ Frontend/src/components/shared/Navbar/NavLinks.jsx
@@ -1,5 +1,20 @@
 import { Box, Link } from "@mui/material";
 
+const navLinks = [
+  {
+    label: "Sell Your Designs",
+    url: "/about/selling",
+  },
+  {
+    label: "Signup",
+    url: "/signup",
+  },
+  {
+    label: "Login",
+    url: "/auth/login",
+  },
+];
+
 export default function NavLinks() {
   return (
     <Box>
