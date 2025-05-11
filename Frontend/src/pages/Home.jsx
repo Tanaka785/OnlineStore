e4aae42bd3dacdb57d45af4f732a18ea08f7c37a
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-11 16:03:39 +0200

    `Update import path for Navbar component in Home.jsx`

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import Navbar from "../components/shared/Navbar";
+import Navbar from "../components/shared/Navbar/Navbar";
 
 export default function Homepage() {
   return (
