69f96cfc03cacaa89bef1f0a85e88a7dc41df16e
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-11 14:42:18 +0200

    `Refactor Logo component: update Box and Typography styles`

--- Frontend/src/components/shared/Logo.jsx
+++ Frontend/src/components/shared/Logo.jsx
@@ -1,12 +1,20 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
+
 export default function Logo() {
   return (
-    <Box sx={{ display: "flex", flexGrow: 1 }}>
-      <IconButton>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 1,
+      }}
+    >
+      <IconButton sx={{ padding: 0 }}>
         <Avatar src="/webvibes.png" />
       </IconButton>
-      <Typography variant="h6" color="primary">
+      <Typography variant="h6" color="primary" sx={{ marginLeft: 1 }}>
         WebVibes
       </Typography>
     </Box>
