f5734adbfcb02fe3ae6541471449deb768aff483
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-11 15:12:43 +0200

    `Update SearchField component with additional styles`

--- Frontend/src/components/shared/SearchField.jsx
+++ Frontend/src/components/shared/SearchField.jsx
@@ -1,5 +1,16 @@
 import { Box } from "@mui/material";
 
 export default function SearchField() {
-  return <Box sx={{ color: "primary.main" }}>Hie</Box>;
+  return (
+    <Box
+      sx={{
+        color: "primary.main",
+        justifyContent: "center",
+        alignItems: "center",
+        flexGrow: 1,
+      }}
+    >
+      Hie
+    </Box>
+  );
 }
