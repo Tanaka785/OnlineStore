fc2bf704f0a71744e3a7b43b47b9f11cef5de2fd
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-12 16:43:22 +0200

    `Added Link component and modified JSX structure in Signup.jsx`

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,5 +1,15 @@
 import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function SignupPage() {
-  return <Box>This is the signup page.</Box>;
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ display: "grid", width: "100%", border: "1px solid blue" }}>
+        <Box sx={{ height: "100%", marginLeft: "auto" }}>
+          <Link>Hie</Link>
+          <Link>Hie</Link>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
