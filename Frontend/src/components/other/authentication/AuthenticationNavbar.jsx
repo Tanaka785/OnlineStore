3674e092fdc81752e1b3136cc810368f30d4632c
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-12 16:52:22 +0200

    `Refactor Authentication Navbar: Extract into separate component and reuse in Signup page`

--- Frontend/src/components/other/authentication/AuthenticationNavbar.jsx
+++ Frontend/src/components/other/authentication/AuthenticationNavbar.jsx
@@ -1,3 +1,27 @@
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export default function AuthenticationNavbar() {
-  return <>I am the authentication Navbar.</>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        border: "1px solid blue",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          height: "100%",
+          marginLeft: "auto",
+          marginRight: 4,
+        }}
+      >
+        <Link>Sell Your Designs</Link>
+        <Link>Login</Link>
+      </Box>
+    </Box>
+  );
 }
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,29 +1,11 @@
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import AuthenticationNavbar from "../components/other/authentication/AuthenticationNavbar";
 
 export default function SignupPage() {
   return (
     <Box sx={{ display: "grid", width: "100%" }}>
-      <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          border: "1px solid blue",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            height: "100%",
-            marginLeft: "auto",
-            marginRight: 4,
-          }}
-        >
-          <Link>Sell Your Designs</Link>
-          <Link>Login</Link>
-        </Box>
-      </Box>
+      <AuthenticationNavbar />
     </Box>
   );
 }
