39abf67c0d048360b54039e82c783c807dd1198c
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-12 16:45:09 +0200

    `Update Signup page links and layout`

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -5,9 +5,11 @@ export default function SignupPage() {
   return (
     <Box sx={{ width: "100%" }}>
       <Box sx={{ display: "grid", width: "100%", border: "1px solid blue" }}>
-        <Box sx={{ height: "100%", marginLeft: "auto" }}>
-          <Link>Hie</Link>
-          <Link>Hie</Link>
+        <Box
+          sx={{ display: "flex", gap: 2, height: "100%", marginLeft: "auto" }}
+        >
+          <Link>Sell Your Designs</Link>
+          <Link>Login</Link>
         </Box>
       </Box>
     </Box>
