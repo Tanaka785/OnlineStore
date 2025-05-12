0baa0abdbf6576938bb508e69e2fe27f88b930ac
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-12 16:48:51 +0200

    `Refactor SignupPage JSX: update Box styles and add marginRight`

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -3,10 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function SignupPage() {
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ display: "grid", width: "100%", border: "1px solid blue" }}>
+    <Box sx={{ display: "grid", width: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          border: "1px solid blue",
+        }}
+      >
         <Box
-          sx={{ display: "flex", gap: 2, height: "100%", marginLeft: "auto" }}
+          sx={{
+            display: "flex",
+            gap: 2,
+            height: "100%",
+            marginLeft: "auto",
+            marginRight: 4,
+          }}
         >
           <Link>Sell Your Designs</Link>
           <Link>Login</Link>
