45d24115645523aa9e3fe7382c363a0c5a4bbac3
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 15:54:03 +0200

    adds the textFields inside a formcontrol in AuthenticationFields.jsx

--- Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
+++ Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
@@ -1,13 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, FormControl } from "@mui/material";
 import StandardTextField from "./StandardTextField";
 import CenteredBox from "../CenteredBox";
 
 export default function AuthenticationFields() {
   return (
     <CenteredBox sx={{ gap: 4, marginBlock: 4 }}>
-      <StandardTextField label="Email" />
-      <StandardTextField label="Username" />
-      <StandardTextField label="Password" />
+      <FormControl sx={{ width: "100%" }}>
+        <StandardTextField label="Email" />
+        <StandardTextField label="Username" />
+        <StandardTextField label="Password" />
+      </FormControl>
     </CenteredBox>
   );
 }
