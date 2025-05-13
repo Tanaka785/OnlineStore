f885ce62df5309e3cbeb0b5e0a786be565e88480
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 15:54:51 +0200

    puts gap property to formcontrol

--- Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
+++ Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
@@ -4,8 +4,8 @@ import CenteredBox from "../CenteredBox";
 
 export default function AuthenticationFields() {
   return (
-    <CenteredBox sx={{ gap: 4, marginBlock: 4 }}>
-      <FormControl sx={{ width: "100%" }}>
+    <CenteredBox sx={{ marginBlock: 4 }}>
+      <FormControl sx={{ width: "100%", gap: 4 }}>
         <StandardTextField label="Email" />
         <StandardTextField label="Username" />
         <StandardTextField label="Password" />
