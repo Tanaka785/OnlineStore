098342c652e3a0d47c4d81d89be8df6263e05ce5
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 13:05:25 +0200

    sets the width of the containing box in Promo.jsx to be 36% of its parent.

--- Frontend/src/components/other/authentication/signup/Promo.jsx
+++ Frontend/src/components/other/authentication/signup/Promo.jsx
@@ -8,14 +8,15 @@ export default function Promo(params) {
         marginTop: 10,
         border: "1px solid red",
         placeItems: "center",
+        width: "35%",
       }}
     >
       <Typography component={"div"} variant="h5">
         Join WebVibes
       </Typography>
-      <Typography>
+      <Typography component="div" variant="body1" sx={{ textAlign: "center" }}>
         Sign up as a customer for 25% off your first order. Your coupon will be
-        emailed after sign up
+        emailed after sign up.
       </Typography>
     </Box>
   );
