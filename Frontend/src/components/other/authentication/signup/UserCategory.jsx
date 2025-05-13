e9c72b8e2a3a5b4b65864ac64c25c4bf2cc9dd42
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 15:01:02 +0200

    adds Radio buttons to let users choose their category when signing up

--- Frontend/src/components/other/authentication/signup/UserCategory.jsx
+++ Frontend/src/components/other/authentication/signup/UserCategory.jsx
@@ -1,9 +1,16 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import CenteredBox from "./CenteredBox";
 import styled from "@emotion/styled";
 import { useTheme } from "@emotion/react";
 
-const HalfBox = styled(Box)(({ theme }) => ({
+const HalfBoxFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
   flex: 1,
   display: "grid",
   placeItems: "center",
@@ -15,8 +22,37 @@ export default function UserCategory() {
   const theme = useTheme();
   return (
     <CenteredBox sx={{ display: "flex", marginTop: 4 }}>
-      <HalfBox>HalfBox 1</HalfBox>
-      <HalfBox>HalfBox 2</HalfBox>
+      <FormControl
+        sx={{
+          //   border: "1px solid blue",
+          width: "100%",
+        }}
+      >
+        <FormLabel sx={{ width: "100%", textAlign: "center" }}>
+          Choose Your Category
+        </FormLabel>
+        <RadioGroup
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            marginLeft: 1.5,
+            // TODO find out why the formcontrolLabels are overlapping the form
+            width: "100%",
+          }}
+        >
+          <HalfBoxFormControlLabel
+            value="option1"
+            control={<Radio />}
+            label="Artist Signup"
+          />
+          <HalfBoxFormControlLabel
+            component="div"
+            value="option2"
+            control={<Radio />}
+            label="Customer Signup"
+          />
+        </RadioGroup>
+      </FormControl>
     </CenteredBox>
   );
 }
