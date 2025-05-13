e9147ffff3a970b27fefe1abbfed18a6996c1a83
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 13:01:06 +0200

    sets placeItems "center" for the containing box in Promo.jsx

--- Frontend/src/components/other/authentication/signup/Promo.jsx
+++ Frontend/src/components/other/authentication/signup/Promo.jsx
@@ -2,7 +2,14 @@ import { Box, Typography } from "@mui/material";
 
 export default function Promo(params) {
   return (
-    <Box sx={{ display: "grid", marginTop: 10, border: "1px solid red" }}>
+    <Box
+      sx={{
+        display: "grid",
+        marginTop: 10,
+        border: "1px solid red",
+        placeItems: "center",
+      }}
+    >
       <Typography component={"div"} variant="h5">
         Join WebVibes
       </Typography>
