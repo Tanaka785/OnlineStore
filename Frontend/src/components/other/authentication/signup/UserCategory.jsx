653f97cdb866dd4b27fc2989e1d20d6c4f198151
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 14:01:23 +0200

    creates HalfBox reusable box that has flex: 1 to allow the HalfBoxes to take the same amount of space in the parent element

--- Frontend/src/components/other/authentication/signup/UserCategory.jsx
+++ Frontend/src/components/other/authentication/signup/UserCategory.jsx
@@ -1,3 +1,16 @@
+import { Box } from "@mui/material";
+import CenteredBox from "./CenteredBox";
+import styled from "@emotion/styled";
+
+const HalfBox = styled(Box)({
+  flex: 1,
+});
+
 export default function UserCategory() {
-  return <>Categories</>;
+  return (
+    <CenteredBox sx={{ display: "flex" }}>
+      <HalfBox>HalfBox 1</HalfBox>
+      <HalfBox>HalfBox 2</HalfBox>
+    </CenteredBox>
+  );
 }
