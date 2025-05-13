f87f806e6fc396f4f3f532436c4b7577e9c29749
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 13:10:38 +0200

    adds CenteredBox.jsx file

---
+++ Frontend/src/components/other/authentication/signup/CenteredBox.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+export default function CentereBox() {
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        placeItems: "center",
+        width: "35%",
+      }}
+    />
+  );
+}
