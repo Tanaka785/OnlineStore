27916e9073dbf8f3ac50f6038a6503de745a8aa2
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 12:58:41 +0200

    adds Typography component with "Join WebVibes" text.

--- Frontend/src/components/other/authentication/signup/Promo.jsx
+++ Frontend/src/components/other/authentication/signup/Promo.jsx
@@ -1,9 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Promo(params) {
   return (
     <Box sx={{ display: "flex", marginTop: 10, border: "1px solid red" }}>
-      I am the promo text.
+      <Typography component={"div"} variant="h5">
+        Join WebVibes
+      </Typography>
     </Box>
   );
 }
