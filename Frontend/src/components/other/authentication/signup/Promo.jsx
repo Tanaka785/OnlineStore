b08a9391f37ff0510bd152425c7fb19e85fe9f23
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 13:00:16 +0200

    adds another typography component for the promo msg, and made the containing box display "grid:

--- Frontend/src/components/other/authentication/signup/Promo.jsx
+++ Frontend/src/components/other/authentication/signup/Promo.jsx
@@ -2,9 +2,13 @@ import { Box, Typography } from "@mui/material";
 
 export default function Promo(params) {
   return (
-    <Box sx={{ display: "flex", marginTop: 10, border: "1px solid red" }}>
+    <Box sx={{ display: "grid", marginTop: 10, border: "1px solid red" }}>
       <Typography component={"div"} variant="h5">
         Join WebVibes
+      </Typography>
+      <Typography>
+        Sign up as a customer for 25% off your first order. Your coupon will be
+        emailed after sign up
       </Typography>
     </Box>
   );
