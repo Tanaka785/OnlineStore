c93a29154b2f14afecf68dc1e53e10fbddbe07c3
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 12:15:34 +0200

    gives the promo component marginTop of 10

--- Frontend/src/components/other/authentication/signup/Promo.jsx
+++ Frontend/src/components/other/authentication/signup/Promo.jsx
@@ -1,3 +1,9 @@
+import { Box } from "@mui/material";
+
 export default function Promo(params) {
-  return <>I am the promo text</>;
+  return (
+    <Box sx={{ marginTop: 10, border: "1px solid red" }}>
+      I am the promo text.
+    </Box>
+  );
 }
