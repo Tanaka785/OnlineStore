e0060790a7ef10e02f8c1b8d3f4edaad089c0f64
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 15:56:47 +0200

    adds checkbox to the form in authenticationFields.jsx

--- Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
+++ Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
@@ -1,14 +1,18 @@
-import { Box, FormControl } from "@mui/material";
+import { Box, Checkbox, FormControl, FormControlLabel } from "@mui/material";
 import StandardTextField from "./StandardTextField";
 import CenteredBox from "../CenteredBox";
 
 export default function AuthenticationFields() {
   return (
     <CenteredBox sx={{ marginBlock: 4 }}>
-      <FormControl sx={{ width: "100%", gap: 4 }}>
+      <FormControl sx={{ width: "100%", gap: 3 }}>
         <StandardTextField label="Email" />
         <StandardTextField label="Username" />
         <StandardTextField label="Password" />
+        <FormControlLabel
+          control={<Checkbox />}
+          label="Email me special offers and artist news."
+        />
       </FormControl>
     </CenteredBox>
   );
