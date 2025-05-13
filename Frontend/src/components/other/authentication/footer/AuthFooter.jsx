420d1248b53ee02883567ca7a16b5467816f022f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-13 21:29:12 +0200

    adds initial styles to the footer in AuthFooter.jsx

--- Frontend/src/components/other/authentication/footer/AuthFooter.jsx
+++ Frontend/src/components/other/authentication/footer/AuthFooter.jsx
@@ -1,3 +1,18 @@
+import { Box } from "@mui/material";
+
 export default function AuthFooter() {
-  return <>I am the footer.</>;
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        bottom: 0,
+        width: "100%",
+        border: "1px solid blue",
+        minHeight: 200,
+        placeItems: "center",
+      }}
+    >
+      I am the footer.
+    </Box>
+  );
 }
