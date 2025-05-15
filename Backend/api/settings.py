f879647ba50fc978865656d5473c60f60970fe6f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-15 16:42:29 +0200

    "Added 'rest_framework' and 'users' to INSTALLED_APPS in settings.py"

--- Backend/api/settings.py
+++ Backend/api/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External
     'rest_framework',
+    # Internal
+    'users',
 ]
 
 MIDDLEWARE = [
