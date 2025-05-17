9f0a5ba84ee47758e98b8db80817dfe20b1834b8
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-17 12:36:11 +0200

    adds functions to handle the textfields state.

--- Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
+++ Frontend/src/components/other/authentication/signup/authentication-fields/AuthenticationFields.jsx
@@ -12,15 +12,29 @@ import SubmitButton from "./SubmitButton";
 import { LOGIN } from "../../../../../constants/routes";
 
 export default function AuthenticationFields({ selectedCategory, state, setState }) {
-  console.log(state.email, state.usernameOrShopName, state.Password);
+  console.log(state);
+
+  const handleEmailChange = () => {
+    console.log("I Email");
+  }
+
+  const handleUsernameOrShopNameChange = () => {
+    console.log("I UsernameOrShopName");
+  };
+
+  const handlePasswordChange = () => {
+    console.log("I Password");
+  };
+
   return (
     <CenteredBox sx={{ marginBlock: 4 }}>
       <FormControl sx={{ width: "100%", gap: 3 }}>
-        <StandardTextField label="Email" />
+        <StandardTextField label="Email" onChange={handleEmailChange} />
         <StandardTextField
           label={selectedCategory === "Customer" ? "Username" : "Shop name"}
+          onChange={handleUsernameOrShopNameChange}
         />
-        <StandardTextField label="Password" />
+        <StandardTextField label="Password" onChange={handlePasswordChange} />
         <FormControlLabel
           control={<Checkbox color="secondary" />}
           label="Email me special offers and artist news."
