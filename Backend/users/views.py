2171d997c4759ae4f3fd768b83e407015d53a2b0
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-19 15:55:11 +0200

    Add success message to user registration response in SignupView

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -6,3 +6,11 @@ from .models import User
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignupSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        response.data = {
+            "message": "User registration successful",
+            "status": "success"
+        }
+        return response
