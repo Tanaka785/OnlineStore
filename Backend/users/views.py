bdfef7b7142c7c2cc67ba597a638269b41845c26
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-19 14:17:35 +0200

    Add SignupView for user registration using SignupSerializer in views.py

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import SignupSerializer
+from .models import User
 
-# Create your views here.
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
