7d088b78c031d495e8068558df5f2b8f80784b03
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-20 11:59:46 +0200

    Enhance User model by adding userNameOrShopname as the unique identifier, removing the default username field, and updating the string representation. Include TODOs for admin interface improvements regarding customer_type and user fields visibility.

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -8,7 +8,13 @@ class User(AbstractUser):
         ("artist", "Artist"),
         ("customer", "Customer"),
     ]
-
+    userNameOrShopname = models.CharField(
+        max_length=150, unique=True, null=False, blank=False
+    )
+    username = None
+    USERNAME_FIELD = "userNameOrShopname"
+    # TODO in the admin interface, or why am i being able to save a user without customer_type in admin interface.
+    # TODO Plus i need all the fields for a user to show when i try saving a user in the admin interface.
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -23,4 +29,4 @@ class User(AbstractUser):
     )
 
     def __str__(self):
-        return self.username
+        return self.userNameOrShopname
