595e14bfd83f571915f9d8513b08951393e32bbf
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-20 22:05:50 +0200

    removes unused imports in serializers.py

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError as DjangoValidationError
 from .models import User
 
 
