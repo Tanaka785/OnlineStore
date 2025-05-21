432f3104412d40c7a0fbcb66c7a91385d2b3ce6f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-21 13:37:40 +0200

    Add AuthHeading to LoginPage for enhanced user guidance and navigation to Signup

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,6 +2,9 @@ import AuthBox from "../components/other/authentication/shared/AuthBox";
 import AuthenticationNavbar from "../components/other/authentication/shared/AuthenticationNavbar";
 import AuthCenteredBox from "../components/other/authentication/shared/AuthCenteredBox";
 import AuthenticationHeader from "../components/other/authentication/shared/AuthenticationHeader";
+import AuthHeading from "../components/other/authentication/signup/Promo";
+import { SIGNUP } from "../constants/routes";
+import { Link } from "@mui/material";
 
 export default function LoginPage() {
   return (
@@ -10,6 +13,14 @@ export default function LoginPage() {
 
       <AuthCenteredBox>
         <AuthenticationHeader />
+        <AuthHeading
+          primaryText="Login"
+          secondaryText={
+            <>
+              Need an account? <Link href={SIGNUP}><b>Sign Up</b></Link>
+            </>
+          }
+        />
       </AuthCenteredBox>
     </AuthBox>
   );
