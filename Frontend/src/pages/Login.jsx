dbe8dd345256cedc3c5ccd1fafd0e64acffe2f99
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-21 13:04:57 +0200

    Enhance LoginPage by adding AuthenticationNavbar for improved navigation options.

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/material";
 import AuthBox from "../components/other/authentication/shared/AuthBox";
+import AuthenticationNavbar from "../components/other/authentication/shared/AuthenticationNavbar";
 
 export default function LoginPage() {
 
   return (
   <AuthBox>
-    Login here.
+    <AuthenticationNavbar authLink="Signup" />
   </AuthBox>
   );
 }
