0098815807359c39b04afb4c2088c515ae82371f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-24 14:27:09 +0200

    Update login URL to use MyTokenObtainPairView for enhanced JWT authentication, allowing login via username or email.

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignupView, UserListView
+from .views import SignupView, UserListView, MyTokenObtainPairView
 
 urlpatterns = [
     path("", UserListView.as_view(), name="users"),
     path("register/", SignupView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", MyTokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
