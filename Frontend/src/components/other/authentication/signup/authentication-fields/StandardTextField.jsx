bbfd9a0a632cdddaf1d97d5c66230ca4c4015a08
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-24 13:54:47 +0200

    Add password visibility toggle to StandardTextField component

--- Frontend/src/components/other/authentication/signup/authentication-fields/StandardTextField.jsx
+++ Frontend/src/components/other/authentication/signup/authentication-fields/StandardTextField.jsx
@@ -1,14 +1,48 @@
-import { TextField } from "@mui/material";
-import { forwardRef } from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { forwardRef, useState } from "react";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const StandardTextField = forwardRef((props, ref) => {
+  const { type, ...rest } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = type === "password";
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <TextField
       variant="standard"
       fullWidth
       sx={{ paddingBlock: 2 }}
-      {...props}
+      {...rest}
+      type={isPassword ? (showPassword ? "text" : "password") : type}
       inputRef={ref}
+      InputProps={
+        isPassword
+          ? {
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? "Hide password" : "Show password"
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    tabIndex={-1}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }
+          : rest.InputProps
+      }
     />
   );
 });
