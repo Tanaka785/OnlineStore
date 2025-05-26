97d22763f467ac8e1114360300c8e445776bd977
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-26 14:09:30 +0200

    Add ProductViewSet and URL routing for product-related endpoints in the products app.

--- Backend/products/views.py
+++ Backend/products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
