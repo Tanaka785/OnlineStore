77b8760a47ede364f8f931b1dc24a026f64e5a36
Tanaka785 <tanakachanetsa204@gmail.com>
2025-05-26 15:08:36 +0200

    Add ProductVariationInline to ProductAdmin and create ProductVariationAdmin for better management of product variations in the admin interface.

--- Backend/products/admin.py
+++ Backend/products/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductVariation
+
+
+class ProductVariationInline(admin.TabularInline):
+    model = ProductVariation
+    extra = 1
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "image", "created_at")
+    inlines = [ProductVariationInline]
+
+
+@admin.register(ProductVariation)
+class ProductVariationAdmin(admin.ModelAdmin):
+    list_display = ("product", "size", "color", "price", "stock")
