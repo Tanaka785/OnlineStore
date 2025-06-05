eac0a473360a42fa1b06416281b8e61906f17117
Tanaka785 <tanakachanetsa204@gmail.com>
2025-06-05 12:16:49 +0200

    Add menu and search icons to CartNavbar for enhanced navigation

--- Frontend/src/components/other/cartpage/CartNavbar.jsx
+++ Frontend/src/components/other/cartpage/CartNavbar.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 
 function CartNavbar() {
   return (
     <AppBar position="static" sx={{}}>
       <Toolbar>
+        <IconButton
+          edge="start"
+          color="secondary.main"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <IconButton
+          edge="start"
+          color="secondary.main"
+          aria-label="search"
+          sx={{ mr: 2 }}
+        >
+          <SearchIcon />
+        </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Your Cart
         </Typography>
