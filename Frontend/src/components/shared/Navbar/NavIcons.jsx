edaa15e23e3ecb16aa8b3d461a364eefa6e79655
Tanaka785 <tanakachanetsa204@gmail.com>
2025-06-11 16:34:16 +0200

    Enhance NavIcons component to handle authentication for navigation
    - Added LOGIN route to NavIcons for redirecting unauthenticated users when accessing the Cart.
    - Implemented handleIconClick function to manage navigation based on authentication state.
    - Wrapped icon components in IconButton for improved user interaction and accessibility.

--- Frontend/src/components/shared/Navbar/NavIcons.jsx
+++ Frontend/src/components/shared/Navbar/NavIcons.jsx
@@ -1,24 +1,41 @@
 import { Box, IconButton, Tooltip } from "@mui/material";
-import { CART, WISHLISTS } from "../../../constants/routes";
+import { CART, WISHLISTS, LOGIN } from "../../../constants/routes";
 import Cart from "../Cart";
 import WishLists from "../WishLists";
+import { useAuth } from "../../../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const navIcons = [
   {
     label: "WishLists",
     icon: WishLists,
+    route: WISHLISTS,
   },
   {
     label: "Cart",
     icon: Cart,
+    route: CART,
   },
 ];
 
 export default function NavIcons() {
+  const { authToken } = useAuth();
+  const navigate = useNavigate();
+
+  const handleIconClick = (route) => {
+    if (route === CART && !authToken) {
+      navigate(LOGIN);
+    } else {
+      navigate(route);
+    }
+  };
+
   return (
     <Box sx={{ marginInline: 2, display: "flex", gap: 2 }}>
       {navIcons.map((icon) => (
-        <icon.icon key={icon.label} />
+        <IconButton key={icon.label} onClick={() => handleIconClick(icon.route)}>
+          <icon.icon />
+        </IconButton>
       ))}
     </Box>
   );
